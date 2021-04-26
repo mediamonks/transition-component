498a4cb90d627f2a2f317cfe3ce18733471d48a1
Lars van Braam <lars@mediamonks.com>
2021-04-26 09:20:42 +0200

    Display the transition events on the app

--- examples/muban/src/App.template.ts
+++ examples/muban/src/App.template.ts
@@ -4,24 +4,24 @@ import { fooTemplate } from './components/foo/Foo.template';
 export function appTemplate(): string {
   return html`<section data-component="app">
     <span class="badge bg-danger component-label">app-component</span>
-    ${fooTemplate('foo')}
+    <div style="overflow: hidden">${fooTemplate('foo')}</div>
     <div class="container mt-5">
       <div class="row">
         <div class="col-3">
           <h4>Controls</h4>
           <div class="btn-group" role="group" aria-label="Basic outlined example">
-            <button type="button" class="btn btn-outline-primary" data-ref="transition-in">
+            <button type="button" class="btn btn-outline-primary" data-ref="transition-in-button">
               Transition In
             </button>
-            <button type="button" class="btn btn-outline-primary" data-ref="transition-out">
+            <button type="button" class="btn btn-outline-primary" data-ref="transition-out-button">
               Transition Out
             </button>
           </div>
         </div>
         <div class="col">
           <h4>Events</h4>
-          <div class="container bg-dark text-light p-3" data-ref="events">
-            <span class="badge bg-warning">TODO</span> Display transition events
+          <div class="container p-0" data-ref="events" style="max-height: 400px; overflow-y:auto;">
+            <!-- This will be replaced with bindings -->
           </div>
         </div>
       </div>
--- examples/muban/src/App.ts
+++ examples/muban/src/App.ts
@@ -1,12 +1,24 @@
-import { bind, defineComponent, refComponent } from '@muban/muban';
+import {
+  bind,
+  bindTemplate,
+  computed,
+  defineComponent,
+  onMounted,
+  ref,
+  refComponent,
+  shallowRef,
+} from '@muban/muban';
 import { useGlobalTransitionContext } from '@mediamonks/muban-transition-component';
+import { html } from '@muban/template';
+import type { TransitionController } from '@mediamonks/core-transition-component';
 import { Foo } from './components/foo/Foo';
+
 
 export const App = defineComponent({
   name: 'app',
   refs: {
-    transitionIn: 'transition-in',
-    transitionOut: 'transition-out',
+    transitionInButton: 'transition-in-button',
+    transitionOutButton: 'transition-out-button',
     events: 'events',
     foo: refComponent(Foo, {
       ref: 'foo',
@@ -14,16 +26,55 @@ export const App = defineComponent({
   },
   setup({ refs }) {
     const transitionContext = useGlobalTransitionContext();
+    const transitionController = shallowRef<TransitionController | null>(null);
+    const events = ref<Array<{ value: string; time: string }>>([]);
+
+    const eventListeners = {
+      onStart: () => {
+        events.value = [...events.value, { value: 'onStart', time: new Date().toISOString() }];
+      },
+      onComplete: () => {
+        events.value = [...events.value, { value: 'onComplete', time: new Date().toISOString() }];
+      },
+    };
+
+    onMounted(() => {
+      transitionController.value = transitionContext.getController(refs.foo);
+      transitionController.value.transitionIn(eventListeners);
+    });
 
     return [
-      bind(refs.transitionIn, {
+      bindTemplate(
+        refs.events,
+        computed(() => ({ events: events.value })),
+        (data) => html`<table class="table">
+          <thead class="table-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Event</th>
+              <th scope="col">Timestamp</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${data.events.map(
+              (event, index) => html`<tr>
+                <td>${index + 1}</td>
+                <td><span class="badge bg-success">${event.value}</span></td>
+                <td class="font-monospace">${event.time}</td>
+              </tr>`,
+            )}
+          </tbody>
+        </table>`,
+        { renderImmediate: true },
+      ),
+      bind(refs.transitionInButton, {
         event: {
-          click: () => transitionContext.getController(refs.foo)?.transitionIn(),
+          click: () => transitionController.value?.transitionIn(eventListeners),
         },
       }),
-      bind(refs.transitionOut, {
+      bind(refs.transitionOutButton, {
         event: {
-          click: () => transitionContext.getController(refs.foo)?.transitionOut(),
+          click: () => transitionController.value?.transitionOut(eventListeners),
         },
       }),
     ];
