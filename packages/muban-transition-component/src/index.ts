9905c2a12cedc11ca7092cb6bd83e63e6d762b79
Lars van Braam <lars@mediamonks.com>
2021-04-28 10:42:33 +0200

    Update the main exports of the muban-transition-component

--- packages/muban-transition-component/src/index.ts
+++ packages/muban-transition-component/src/index.ts
@@ -4,3 +4,35 @@ export { usePageTransition } from './hooks/usePageTransition';
 export { usePageTransitioning } from './hooks/usePageTransitioning';
 export { useScrollTransition } from './hooks/useScrollTransition';
 export { useTransitionController } from './hooks/useTransitionController';
+
+// Context
+export { FlowContext } from './context/FlowContext';
+export { TransitionContext } from './context/TransitionContext';
+
+// Utils
+export { transitionRefToElement } from './util/Transition.utils';
+
+// Types
+export type {
+  SetupTransitionSignature,
+  TransitionDirection,
+  SignatureRefElement,
+  SignatureRefCollection,
+  SignatureElement,
+  TransitionOptions,
+  SetupPageTransitionOptions,
+  SetupTransitionOptions,
+  TransitionController,
+  GuardFunction,
+  SetupScrollTransitionOptions,
+  TimelineOptions,
+  TransitionInOptions,
+  TransitionOutOptions,
+} from '@mediamonks/core-transition-component';
+
+export type {
+  TransitionRefCollection,
+  TransitionRefElement,
+  TransitionRef,
+  SetupSignatureElements,
+} from './types/Transition.types';
