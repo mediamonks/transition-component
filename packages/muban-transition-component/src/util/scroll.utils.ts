4bcec5bac0b9f14e23fc7880d8f67f8912478e20
Lars van Braam <lars@mediamonks.com>
2021-07-06 13:33:12 +0200

    Replace the ScrollTrigger that detects the viewport leave with an IntersectionObserver fixes #3

---
+++ packages/muban-transition-component/src/util/scroll.utils.ts
@@ -0,0 +1,46 @@
+type ViewportLeavePosition = 'top' | 'bottom';
+
+let viewportLeaveObserver: IntersectionObserver | null = null;
+const observerCallbacks = new Map<Element, (position: ViewportLeavePosition) => void>();
+const observerPositions = new Map<Element, number>();
+
+// Make sure we only have one intersection observer instance that tracks whether or not something is in the viewport,
+// but we still want to be able to polyfill it so create it once it's needed.
+function getViewportLeaveObserver(): IntersectionObserver {
+  if (!viewportLeaveObserver) {
+    viewportLeaveObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const previousY = observerPositions.get(entry.target) ?? 0;
+          const callback = observerCallbacks.get(entry.target);
+
+          // Detect if the entry leaves the viewport and where it leaves the viewport
+          if (!entry.isIntersecting)
+            callback?.(entry.boundingClientRect.y > previousY ? 'bottom' : 'top');
+
+          observerPositions.set(entry.target, entry.boundingClientRect.y);
+        });
+      },
+      {
+        threshold: [0, 1],
+      },
+    );
+  }
+
+  return viewportLeaveObserver;
+}
+
+export function addLeaveViewportObserver(
+  element: Element,
+  onLeaveViewport: (position: ViewportLeavePosition) => void,
+): () => void {
+  const observer = getViewportLeaveObserver();
+  observerCallbacks.set(element, (position) => onLeaveViewport?.(position));
+  observer.observe(element);
+
+  return () => {
+    observer.unobserve(element);
+    observerCallbacks.delete(element);
+    observerPositions.delete(element);
+  };
+}
