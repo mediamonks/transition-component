1f3befcf79e363baa4925d7b3f70b8d32990023c
Twan <twan.braas@mediamonks.com>
2021-09-09 10:30:57 +0200

    add automatic scripts for testing

---
+++ examples/react/playwright.config.ts
@@ -0,0 +1,12 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  retries: 2,
+  use: {
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'on-first-retry',
+  },
+};
+export default config;
