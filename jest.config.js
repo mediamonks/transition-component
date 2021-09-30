39eb37fcc622cc03e9183e04273ea07b80fb4b8e
Leroy Korterink <leroy@mediamonks.com>
2021-09-30 13:15:41 +0200

    Update jest setup and create tests for TransitionPresence component

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    preset: 'ts-jest/presets/js-with-babel',
-    moduleNameMapper: {
-        '^lodash-es$': 'lodash'
-    }
-};
---
+++ packages/react-transition-component/src/components/TransitionPresence.test.tsx
@@ -0,0 +1,110 @@
+import '@testing-library/jest-dom';
+import { act, render } from '@testing-library/react';
+import type { ReactNode } from 'react';
+import { useRef } from 'react';
+import { useLeaveTransition } from '../hooks/useLeaveTransition';
+import { useTransitionController } from '../hooks/useTransitionController';
+import { unwrapRefs } from '../lib/unwrapRefs';
+import { TransitionPresence } from './TransitionPresence';
+
+interface TransitionComponentProps {
+  children: ReactNode;
+}
+
+function TransitionComponent({ children }: TransitionComponentProps) {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const transitionController = useTransitionController(() => ({
+    refs: {
+      divRef,
+    },
+    setupTransitionOutTimeline(timeline, refs) {
+      const { divRef: divElement } = unwrapRefs(refs);
+
+      timeline.fromTo(
+        divElement,
+        {
+          opacity: 1,
+        },
+        {
+          opacity: 0,
+          duration: 0.1,
+        },
+      );
+    },
+  }));
+
+  useLeaveTransition(transitionController);
+
+  return <div ref={divRef}>{children}</div>;
+}
+
+describe('<TransitionPresence />', () => {
+  it('Should render children on first render', () => {
+    const { container } = render(
+      <TransitionPresence>
+        <TransitionComponent>Component 1</TransitionComponent>
+      </TransitionPresence>,
+    );
+
+    expect(container.firstChild).toHaveTextContent('Component 1');
+  });
+
+  it('Should render new children after leave transition on rerender', async () => {
+    const { container, rerender } = render(
+      <TransitionPresence>
+        <TransitionComponent>Component 1</TransitionComponent>
+      </TransitionPresence>,
+    );
+
+    expect(container).toHaveTextContent('Component 1');
+
+    await act(async () => {
+      rerender(
+        <TransitionPresence>
+          <TransitionComponent>Component 2</TransitionComponent>
+        </TransitionPresence>,
+      );
+
+      // Check if old component is still mounted
+      expect(container).toHaveTextContent('Component 1');
+
+      // Wait for transition
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    });
+
+    // Old children should be unmounted after transition
+    expect(container).not.toHaveTextContent('Component 1');
+    expect(container).toHaveTextContent('Component 2');
+  });
+
+  it('Should render children on first render with crossFlow', () => {
+    const { container } = render(
+      <TransitionPresence crossFlow>
+        <TransitionComponent>Component 1</TransitionComponent>
+      </TransitionPresence>,
+    );
+
+    expect(container.firstChild).toHaveTextContent('Component 1');
+  });
+
+  it('Should render new children immediately on rerender with crossFlow', () => {
+    const { container, rerender } = render(
+      <TransitionPresence crossFlow>
+        <TransitionComponent>Component 1</TransitionComponent>
+      </TransitionPresence>,
+    );
+
+    expect(container).toHaveTextContent('Component 1');
+
+    rerender(
+      <TransitionPresence crossFlow>
+        <TransitionComponent>Component 2</TransitionComponent>
+      </TransitionPresence>,
+    );
+
+    // Should have both children now
+    expect(container).toHaveTextContent('Component 1');
+    expect(container).toHaveTextContent('Component 2');
+  });
+});
