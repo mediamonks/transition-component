515b162e5110c7f68fc0e3850fd11d30492317ab
Lars van Braam <lars@mediamonks.com>
2022-08-03 14:49:04 +0200

    Remove the viewport stuff because scrollTrigger does it for us

--- packages/muban-transition-component/src/hooks/useScrollTransition.ts
+++ packages/muban-transition-component/src/hooks/useScrollTransition.ts
@@ -1,11 +1,9 @@
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import type { ComponentFactory, ComponentRef, ElementRef } from '@muban/muban';
-import { onUnmounted } from '@muban/muban';
 import { useTransitionController } from './useTransitionController';
 import type { SetupTransitionOptions } from '../types/transition.types';
 import { defaultScrollTriggerVariables, useScrollContext } from '../context/ScrollTriggerContext';
-import { addLeaveViewportObserver } from '../utils/scroll.utils';
 import { unwrapRef } from '../utils/ref.utils';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -33,14 +31,6 @@ export function useScrollTransition<T>(
       },
     }),
   });
-
-  const removeLeaveViewportObserver = addLeaveViewportObserver(trigger, (position) => {
-    if (!scrollTrigger?.scrub && !scrollTrigger?.once && position === 'bottom') {
-      transitionController.getTimeline('in', true)?.pause(0, false);
-    }
-  });
-
-  onUnmounted(removeLeaveViewportObserver);
 
   return transitionController;
 }
