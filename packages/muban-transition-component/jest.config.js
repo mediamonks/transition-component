aeb935d6862bc58f5f736229c010896649b9cfd7
Leroy Korterink <leroy@mediamonks.com>
2022-12-22 09:07:00 +0100

    Use typescript for `jest` config and move `typescript` to devDependencies

--- packages/muban-transition-component/jest.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef, @typescript-eslint/naming-convention, unicorn/prefer-module */
-module.exports = {
-  preset: 'ts-jest/presets/js-with-babel',
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '^lodash-es$': 'lodash',
-  },
-};
