2cae53c158db95375c6268fc4631c10e79b9371b
Arjan van Wijk <thanarie@gmail.com>
2023-02-21 16:03:45 +0100

    Make sure no out animations are triggered on rerender
    If a TransitionPresence renders again after an out animation has already
    started, it would call the out animations on the newly rendered
    components, causing everything to disappear.
    
    This update makes sure the "transition out" logic will not trigger
    during rendering when the children and deferredChildren haven't changed.

--- packages/react-transition-presence/src/TransitionPresence/TransitionPresence.stories.tsx
+++ packages/react-transition-presence/src/TransitionPresence/TransitionPresence.stories.tsx
@@ -1,4 +1,7 @@
-/* eslint-disable react/no-multi-comp, react/jsx-no-literals */
+/* eslint-disable react/no-multi-comp, react/jsx-no-literals,no-console */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useAnimation } from '@mediamonks/react-animation';
+import gsap from 'gsap';
 import { type ReactElement, useCallback, useEffect, useRef, useState } from 'react';
 import { useBeforeUnmount } from '../useBeforeUnmount/useBeforeUnmount.js';
 import { TransitionPresence } from './TransitionPresence.js';
@@ -9,6 +12,8 @@ export default {
 
 type ChildProps = {
   background: string;
+  duration?: number;
+  delayIn?: number;
   onClick(): void;
 };
 
@@ -16,35 +21,31 @@ type ChildProps = {
 function useRerender(): () => void {
   // eslint-disable-next-line react/hook-use-state
   const [count, setCount] = useState(0);
-  // eslint-disable-next-line no-console
   console.log('rerender', count);
   return () => {
     setCount((previous) => previous + 1);
   };
 }
 
-function Child({ background, onClick }: ChildProps): ReactElement {
+function Child({ background, onClick, duration = 1, delayIn = 0 }: ChildProps): ReactElement {
   const ref = useRef<HTMLButtonElement>(null);
 
+  useAnimation(() => {
+    console.log('animate-in', background);
+    return gsap.fromTo(ref.current, { opacity: 0 }, { opacity: 1, duration, delay: delayIn });
+  }, []);
+
   // show visible animation during "before unmount" lifecycle
-  useBeforeUnmount(() => {
-    const duration = 1000;
-    ref.current?.animate([{ opacity: '0' }], {
-      duration,
-    });
-    // Defer unmounting for 1 second
-    return new Promise((resolve) => {
-      setTimeout(resolve, duration);
-    });
+  useBeforeUnmount(async () => {
+    console.log('animate-out', background);
+    return gsap.fromTo(ref.current, { opacity: 1 }, { opacity: 0, duration });
   }, []);
 
   // show when mounted/unmounted
   useEffect(() => {
-    // eslint-disable-next-line no-console
     console.log('mounted', background);
 
     return () => {
-      // eslint-disable-next-line no-console
       console.log('unmounted', background);
     };
   }, []);
@@ -73,6 +74,7 @@ export function DeferFlow(): ReactElement {
       <TransitionPresence>
         {isRedVisible ? (
           <Child
+            key="red"
             background="red"
             // eslint-disable-next-line react/jsx-no-bind
             onClick={(): void => {
@@ -81,6 +83,7 @@ export function DeferFlow(): ReactElement {
           />
         ) : (
           <Child
+            key="blue"
             background="blue"
             // eslint-disable-next-line react/jsx-no-bind
             onClick={(): void => {
@@ -103,6 +106,7 @@ export function CrossFlow(): ReactElement {
       <TransitionPresence crossFlow>
         {isRedVisible ? (
           <Child
+            key="red"
             background="red"
             // eslint-disable-next-line react/jsx-no-bind
             onClick={(): void => {
@@ -111,6 +115,7 @@ export function CrossFlow(): ReactElement {
           />
         ) : (
           <Child
+            key="blue"
             background="blue"
             // eslint-disable-next-line react/jsx-no-bind
             onClick={(): void => {
@@ -183,6 +188,7 @@ export function StartCompleteCallbacks(): ReactElement {
       >
         {isRedVisible ? (
           <Child
+            key="red"
             background="red"
             // eslint-disable-next-line react/jsx-no-bind
             onClick={(): void => {
@@ -191,6 +197,7 @@ export function StartCompleteCallbacks(): ReactElement {
           />
         ) : (
           <Child
+            key="blue"
             background="blue"
             // eslint-disable-next-line react/jsx-no-bind
             onClick={(): void => {
@@ -202,5 +209,51 @@ export function StartCompleteCallbacks(): ReactElement {
 
       <div style={{ marginTop: 24 }}>Click the square (isRedVisible: {String(isRedVisible)})</div>
     </>
+  );
+}
+
+const initialItems = ['red', 'blue', 'green', 'yellow'];
+export function RerenderUnmountIssue(): ReactElement {
+  const [items, setItems] = useState(() => initialItems);
+  const rerender = useRerender();
+
+  const onReset = useCallback(() => {
+    setItems(initialItems);
+  }, [setItems]);
+  const onNextItem = useCallback(() => {
+    setItems((previous) => {
+      const [first, ...rest] = previous;
+      return [...rest, first];
+    });
+    // This would trigger out animations when
+    // not properly handled in the TransitionPresence
+    setTimeout(() => {
+      rerender();
+    }, 100);
+  }, [setItems, rerender]);
+
+  return (
+    <div>
+      <div style={{ display: 'flex' }}>
+        {items.map((item, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={index} style={{ width: index === 0 ? 400 : 200 }}>
+            <TransitionPresence crossFlow={index <= 1}>
+              <Child
+                /* eslint-disable-next-line react/no-array-index-key */
+                key={item + index}
+                background={item}
+                onClick={onNextItem}
+                duration={2}
+                delayIn={0}
+              />
+            </TransitionPresence>
+          </div>
+        ))}
+      </div>
+      <button type="button" onClick={onReset}>
+        Reset
+      </button>
+    </div>
   );
 }
