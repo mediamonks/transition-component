5f070c5e0f091b05f499d182b989d5104bb16a40
Leroy Korterink <leroy@mediamonks.com>
2023-10-09 02:07:10 +0200

    #148 Make SplitTextWrapper polymorphic and add option to provide any children

--- packages/react-animation/src/SplitTextWrapper/SplitTextWrapper.stories.tsx
+++ packages/react-animation/src/SplitTextWrapper/SplitTextWrapper.stories.tsx
@@ -1,50 +1,217 @@
 /* eslint-disable react/jsx-no-literals */
+import { expect } from '@storybook/jest';
+import type { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
 import gsap from 'gsap';
 import { useCallback, useRef, type ReactElement } from 'react';
 import { useAnimation } from '../useAnimation/useAnimation.js';
 import { SplitTextWrapper } from './SplitTextWrapper.js';
 
-export default {
+const meta = {
   title: 'components/SplitTextWrapper',
+  component: SplitTextWrapper,
+} as Meta;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Children: Story = {
+  render(): ReactElement {
+    const splitTextRef = useRef<SplitText>(null);
+
+    const animation = useAnimation(() => {
+      if (!splitTextRef.current) {
+        return;
+      }
+
+      return gsap.from(splitTextRef.current.lines, {
+        paused: true,
+        y: 20,
+        opacity: 0,
+        duration: 0.2,
+        stagger: 0.05,
+      });
+    }, []);
+
+    const onPlay = useCallback(() => {
+      animation.current?.play(0);
+    }, [animation]);
+
+    return (
+      <>
+        <SplitTextWrapper ref={splitTextRef} variables={{ type: 'lines' }} data-testid="wrapper">
+          Lorem ipsum dolor sit <i>amet consectetur</i>
+          <br /> adipisicing elit. <b>Tenetur perspiciatis</b> eius ea, ratione,
+          <br /> illo molestias, <code data-testid="code">quia sapiente</code> modi quo
+          <br /> molestiae temporibus.
+        </SplitTextWrapper>
+        <button
+          onClick={onPlay}
+          type="button"
+          style={{
+            position: 'relative',
+            marginBlockStart: 20,
+            cursor: 'pointer',
+          }}
+        >
+          Play
+        </button>
+      </>
+    );
+  },
+  async play({ canvasElement }) {
+    const canvas = within(canvasElement);
+    const wrapper = canvas.getByTestId('wrapper');
+
+    expect(wrapper).toBeInTheDocument();
+    expect(wrapper.childElementCount).toEqual(4);
+
+    // Wait 2 ticks for styles to be initialized
+    await new Promise<void>((resolve) => {
+      setTimeout(resolve, 0);
+    });
+    await new Promise<void>((resolve) => {
+      setTimeout(resolve, 0);
+    });
+
+    expect(wrapper.children[0]).toHaveStyle({ opacity: '0' });
+    expect(wrapper.children[3]).toHaveStyle({ opacity: '0' });
+
+    await userEvent.click(canvas.getByText('Play'));
+    await new Promise<void>((resolve) => {
+      setTimeout(resolve, 200 + wrapper.childElementCount * 50);
+    });
+
+    expect(wrapper.children[0]).toHaveStyle({ opacity: '1' });
+    expect(wrapper.children[3]).toHaveStyle({ opacity: '1' });
+  },
 };
 
-export function Example(): ReactElement {
-  const splitTextRef = useRef<SplitText>(null);
+export const DangerouslySetInnerHtml: Story = {
+  render(): ReactElement {
+    const splitTextRef = useRef<SplitText>(null);
 
-  const animation = useAnimation(() => {
-    if (!splitTextRef.current) {
-      return;
-    }
+    const animation = useAnimation(() => {
+      if (!splitTextRef.current) {
+        return;
+      }
 
-    return gsap.from(splitTextRef.current.words, {
-      y: 20,
-      x: 4,
-      opacity: 0,
-      duration: 0.2,
-      stagger: 0.05,
-    });
-  }, []);
+      return gsap.from(splitTextRef.current.lines, {
+        y: 20,
+        opacity: 0,
+        duration: 0.2,
+        stagger: 0.05,
+      });
+    }, []);
 
-  const onReplay = useCallback(() => {
-    animation.current?.play(0);
-  }, [animation]);
+    const onPlay = useCallback(() => {
+      animation.current?.play(0);
+    }, [animation]);
 
-  return (
-    <>
-      <h1>
-        <SplitTextWrapper ref={splitTextRef}>
-          <>
-            Lorem ipsum dolor sit <i>amet consectetur</i>
-            <br /> adipisicing elit. <b>Tenetur perspiciatis</b> eius ea, ratione,
-            <br /> illo molestias, <code>quia sapiente</code> modi quo
-            <br /> molestiae temporibus.
-          </>
+    return (
+      <>
+        <SplitTextWrapper
+          ref={splitTextRef}
+          dangerouslySetInnerHTML={{
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            __html:
+              'Lorem ipsum dolor sit <i>amet consectetur</i> <br /> adipisicing elit. <b>Tenetur perspiciatis</b> eius ea, ratione,<br /> illo molestias, <code>quia sapiente</code> modi quo<br /> molestiae temporibus.',
+          }}
+        />
+        <button
+          onClick={onPlay}
+          type="button"
+          style={{
+            position: 'relative',
+            marginBlockStart: 20,
+            cursor: 'pointer',
+          }}
+        >
+          Play
+        </button>
+      </>
+    );
+  },
+};
+
+export const AsProp: Story = {
+  render(): ReactElement {
+    const splitText1Ref = useRef<SplitText>(null);
+    const splitText2Ref = useRef<SplitText>(null);
+
+    const animation = useAnimation(() => {
+      if (!splitText1Ref.current || !splitText2Ref.current) {
+        return;
+      }
+
+      return gsap
+        .timeline()
+        .from(splitText1Ref.current.lines, {
+          y: 20,
+          x: 4,
+          opacity: 0,
+          duration: 0.2,
+          stagger: 0.1,
+        })
+        .from(
+          splitText2Ref.current.words,
+          {
+            opacity: 0,
+            y: 15,
+            duration: 0.5,
+            ease: 'power2.out',
+            stagger: {
+              from: 'edges',
+              amount: 0.5,
+            },
+          },
+          0.15,
+        );
+    }, []);
+
+    const onPlay = useCallback(() => {
+      animation.current?.play(0);
+    }, [animation]);
+
+    return (
+      <>
+        <SplitTextWrapper ref={splitText1Ref} as="h1" data-testid="heading">
+          I&apos;m an h1 element. <br /> <br />
+          <br /> adipisicing elit. <b>Tenetur perspiciatis</b> eius ea, ratione,
+          <br /> illo molestias, <code data-testid="code">quia sapiente</code> modi quo
+          <br /> molestiae temporibus.
         </SplitTextWrapper>
-      </h1>
+        <SplitTextWrapper
+          ref={splitText2Ref}
+          as="label"
+          htmlFor="value"
+          data-testid="label"
+          style={{ display: 'block' }}
+        >
+          I&apos;m a label element. <br /> <br />
+          <br /> adipisicing elit. <b>Tenetur perspiciatis</b> eius ea, ratione,
+          <br /> illo molestias, <code data-testid="code">quia sapiente</code> modi quo
+          <br /> molestiae temporibus.
+        </SplitTextWrapper>
+        <button
+          onClick={onPlay}
+          type="button"
+          style={{
+            position: 'relative',
+            marginBlockStart: 20,
+            cursor: 'pointer',
+          }}
+        >
+          Play
+        </button>
+      </>
+    );
+  },
+  async play({ canvasElement }) {
+    const canvas = within(canvasElement);
 
-      <button onClick={onReplay} type="button" style={{ cursor: 'pointer' }}>
-        Replay
-      </button>
-    </>
-  );
-}
+    expect(canvas.getByTestId('heading').tagName).toEqual('H1');
+    expect(canvas.getByTestId('label').tagName).toEqual('LABEL');
+  },
+};
--- packages/react-animation/src/SplitTextWrapper/SplitTextWrapper.tsx
+++ packages/react-animation/src/SplitTextWrapper/SplitTextWrapper.tsx
@@ -1,20 +1,49 @@
 import { ensuredForwardRef } from '@mediamonks/react-hooks';
 import gsap from 'gsap';
 import SplitText from 'gsap/SplitText';
-import { type ReactElement } from 'react';
+import {
+  type ComponentProps,
+  type ComponentType,
+  type ReactElement,
+  type RefAttributes,
+} from 'react';
 import { renderToString } from 'react-dom/server';
 
 if (typeof window !== 'undefined') {
   gsap.registerPlugin(SplitText);
 }
 
-export type SplitTextWrapperProps = {
-  children: ReactElement;
+/**
+ * Allowed as prop values
+ */
+type KnownTarget = Exclude<keyof JSX.IntrinsicElements, symbol | object>;
+
+type SplitTextWrapperProps<T extends KnownTarget> = {
+  /**
+   * The SplitText variables
+   * @link https://greensock.com/docs/v3/Plugins/SplitText
+   */
   variables?: SplitText.Vars;
+
+  /**
+   * The element type to render, the default is `div`
+   */
+  as?: T;
 };
 
-export const SplitTextWrapper = ensuredForwardRef<SplitText | null, SplitTextWrapperProps>(
-  ({ children, variables = {} }, ref): ReactElement => {
+/**
+ * Polymorphic component type, necessary to get all the attributes/props for the
+ * as prop component
+ */
+type SplitTextWrapperComponent = <T extends KnownTarget = 'div'>(
+  props: SplitTextWrapperProps<T> &
+    Omit<ComponentProps<T>, keyof SplitTextWrapperProps<T> | 'ref'> &
+    RefAttributes<SplitText | null>,
+) => ReactElement;
+
+// @ts-expect-error polymorphic type is not compatible with ensuredForwardRef function factory
+export const SplitTextWrapper: SplitTextWrapperComponent = ensuredForwardRef(
+  ({ variables = {}, as, children, ...props }, ref) => {
     /**
      * Not using useCallback on purpose so that a new SplitText instance is
      * created whenever this component rerenders the children
@@ -27,15 +56,19 @@ export const SplitTextWrapper = ensuredForwardRef<SplitText | null, SplitTextWra
       ref.current = new SplitText(element, variables);
     };
 
+    const Component = (as ?? 'div') as unknown as ComponentType<unknown>;
+
     return (
-      <div
+      <Component
+        {...props}
+        dangerouslySetInnerHTML={
+          props.dangerouslySetInnerHTML ?? {
+            // eslint-disable-next-line @typescript-eslint/naming-convention, react/jsx-no-useless-fragment
+            __html: renderToString(<>{children}</>),
+          }
+        }
         // eslint-disable-next-line react/jsx-no-bind
         ref={onRef}
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          __html: renderToString(children),
-        }}
       />
     );
   },
